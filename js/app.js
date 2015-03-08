3b24dfde58e255b8d88e8675ca4b03dfde5823a6
lameduz <mr.zenatimehdi@gmail.com>
2015-03-08 22:28:05 +0100

    Last version

--- js/app.js
+++ js/app.js
@@ -1,26 +1,76 @@
-var app = angular.module('pokedex',['ngResource']);
-var pokelist = [{}];
+var pokedex = angular.module('pokedex',['ngRoute','ngResource']);
 
-app.controller('PokedexController',['$scope','$http', function($scope,$http)
+pokedex.config(['$routeProvider',
+	function($routeProvider)
+	{
+		$routeProvider
+			.when('/',{
+				templateUrl: 'layout/home.html',
+				controller: 'HomeController'
+			}).
+			when("/pokedex",{
+				templateUrl:'layout/pokedex.html',
+				controller:'PokedexController'
+			}).
+			when('/pokemon/:pokemonId',{
+				templateUrl: 'layout/pokemon-info.html',
+				controller:'PokemonController'
+			}).
+			otherwise({
+				redirectTo: '/'
+			});
+	}]);
+
+pokedex.controller('HomeController',['$scope',function($scope)
 {
-	$scope.pokedex = []
+	$scope.message = 'WOLLUP LA HOME';
+}]);
 
-	$http({
-		method: 'GET',
-		url: 'http://pokeapi.co/api/v1/pokedex/1/'
-	})
+pokedex.controller('PokedexController',['$scope','$http','$resource', function($scope,$http,$resource)
+{
+	$scope.pokedex = [];
+
+	$scope.message = 'COOL LE POKEDEX BRO';
+	$http.get('app/pokedex.json')
 	.success(function (data, status, headers, config) {
-
-		//$scope.pokemon = data.pokemon;
-		angular.forEach(data.pokemon,function(pokemon,key)
+		angular.forEach(data.objects, function(pokemon,key)
 		{
-			$scope.pokedex.push({info:pokemon});
-		});
-	console.log($scope.pokedex);
-
+			$scope.pokedex.push(pokemon);
+			
+		});		
 	})
 	.error(function (data, status, headers, config) {
 		console.log('ya un blem');
 	});
+	$scope.getEventId = function(idFromClick)
+	{
+		$scope.pokefiche = angular.fromJson(idFromClick.target.attributes.data.value);
+		console.log($scope.pokefiche);
+		angular.forEach($scope.pokefiche.sprites, function(sprite)
+		{
+			$http.get('http://pokeapi.co' + sprite.resource_uri).success(function(data)
+				{
+					$scope.pokesprite = data.image;
+				})
+		})	
+	}
 
 }]);
+		
+		
+	
+		
+		
+			
+
+pokedex.controller('PokemonController',['$scope','$http','$routeParams','$resource',function($scope,$http,$routeParams,$resource)
+{
+	var pkdx_id = $routeParams.pokemonId;
+	$scope.pokemon = $resource('app/pokedex.json');
+	$scope.pokemon = $scope.pokemon.get({pkdx_id: pkdx_id});
+	console.log($scope.pokemon);
+}]);
+
+
+
+
